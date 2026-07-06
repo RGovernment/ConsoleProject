7492300f1197f8eb4d346c2593aa5d85597ee5d0
RGovernment <117322410+RGovernment@users.noreply.github.com>
2026-07-06 16:19:49 +0900

    [변경] JObject 사용 및 관련 기능 제거
    1. JObject 사용 여부 확정으로 인한 불필요한 함수 일괄 제거

--- Model/Enemy.cs
+++ Model/Enemy.cs
@@ -7,6 +7,5 @@ public class Enemy : Character
 {
 	public Enemy(string name, int maxHp, List<Skill> SkillList) : base(name, maxHp, SkillList)
 	{
-		//하하 수정 받아라
 	}
 }
--- Skills/Skill.cs
+++ Skills/Skill.cs
@@ -9,19 +9,19 @@ namespace ConsoleGameFramework.Skills;
 
 public class Skill
 {
-    public JObject SkillData { get; set; }
-    public string Id => SkillData.TryGetValue("id", out JToken? value) ? value.ToString() : "";
-    public string Name => SkillData.TryGetValue("name", out JToken? value) ? value.ToString() : "";
+    public Dictionary<string,object> SkillData { get; set; }
+    public string? Id => SkillData.TryGetValue("id", out object? value) ? value.ToString() : "";
+    public string? Name => SkillData.TryGetValue("name", out object? value) ? value.ToString() : "";
     public int Grade { get; set; }
-    public int Coin => SkillData.TryGetValue("coin", out JToken? value) ? Convert.ToInt32(value) :0;
+    public int Coin => SkillData.TryGetValue("coin", out object? value) ? Convert.ToInt32(value) :0;
 
-    public int AttackPoint => SkillData.TryGetValue("atkPoint", out JToken? value) ? Convert.ToInt32(value) : 0;
-    public int CoinPoint => SkillData.TryGetValue("coinPoint", out JToken? value) ? Convert.ToInt32(value) : 0;
-    public string Description => SkillData.TryGetValue("description", out JToken? value) ? value.ToString() : "";
+    public int AttackPoint => SkillData.TryGetValue("atkPoint", out object? value) ? Convert.ToInt32(value) : 0;
+    public int CoinPoint => SkillData.TryGetValue("coinPoint", out object? value) ? Convert.ToInt32(value) : 0;
+    public string? Description => SkillData.TryGetValue("description", out object? value) ? value.ToString() : "";
     public List<string> SkillEffect { get; set; }
     public List<EffectStatus> EffectList { get; set; }
 
-    public Skill(JObject skillData, int grade, List<string> skillEffect, List<EffectStatus> effect)
+    public Skill(Dictionary<string, object> skillData, int grade, List<string> skillEffect, List<EffectStatus> effect)
     {
         SkillData = skillData;
         Grade = grade;
